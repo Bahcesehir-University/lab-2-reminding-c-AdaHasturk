a9819853ca0df3f654f98399612f1ab6f394cc81
AdaHasturk <sevkiada.hasturk@bahcesehir.edu.tr>
2026-03-10 12:40:14 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -12,9 +12,8 @@ PART 1 — Variables and Arithmetic
 
 int addNumbers(int a, int b)
 {
-    // TODO
-    // Return the sum of a and b
-
+    return a + b;
+    
 }
 
 
@@ -30,6 +29,13 @@ int sumUpToN(int n)
     // TODO
     // Using a loop calculate the sum
     // 1 + 2 + 3 + ... + n
+    
+    int sum = 0;
+    for ( int i = 1; i <= n; i++ ){
+        sum +=i;
+    }
+    return sum;
+    
 }
 
 
@@ -44,6 +50,14 @@ int findMaximum(vector<int> numbers)
 {
     // TODO
     // Return the largest number in the vector
+    
+    int maxValue = numbers[0] ;
+    for ( int i = 1; i < numbers.size(); i++){
+        if(numbers[i] > maxValue){
+            maxValue = numbers[i];
+        }
+    }
+    return maxValue;
 }
 
 
@@ -58,6 +72,12 @@ string reverseString(string text)
 {
     // TODO
     // Return the reversed version of the string
+    
+    string reversed = "";
+    for( int i = text.length() - 1; i >= 0; i--){
+        reversed += text[i];
+    }
+    return reversed;
 }
 
 
@@ -79,6 +99,11 @@ public:
     // TODO
     // Create a constructor that receives
     // name and grade
+    
+    Student(string n, int g){
+        name = n;
+        grade = g;
+    }
 
 
     // TODO
@@ -86,7 +111,12 @@ public:
     // that prints:
     // Name: <name>
     // Grade: <grade>
-
+    void printInfo(){
+        cout << "Name:" << name << endl;
+        cout << "Grade:" << grade << endl;
+    }
+    
+    
 };
 
 
